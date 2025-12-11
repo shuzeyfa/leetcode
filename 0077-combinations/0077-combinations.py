12eef0b0e2c054c815473d1e30250f6e7e06f815
Huzeyfa suleyman <shuzeyfa4@gmail.com>
2025-12-11 08:17:05 +0300

    Time: 141 ms (38.77%), Space: 59.9 MB (14.84%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,14 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        def back(start,comb):
+            if len(comb) == k:
+                res.append(comb[:])
+                return 
+            for i in range(start,n+1):
+                comb.append(i)
+                back(i+1,comb)
+                comb.pop()
+            comb = []
+        back(1,[])
+        return res
