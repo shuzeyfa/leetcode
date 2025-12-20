6b775c28f9ea87c8104b39c940c32c398a6eb2bf
Huzeyfa suleyman <shuzeyfa4@gmail.com>
2025-12-20 10:10:14 +0300

    Time: 3 ms (97.01%), Space: 18.6 MB (56.56%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        left, right = 0, 0
+        result = 0
+        while right<len(nums)-1:
+            far = 0
+            for i in range(left, right + 1):
+                far = max(far, i + nums[i])
+            left = right + 1
+            right = far
+            result += 1
+
+        return result
