e0657d004ca043b4b81f9fd7f659feed3a94663b
Huzeyfa suleyman <shuzeyfa4@gmail.com>
2026-02-17 09:11:32 +0300

    Time: 13 ms (54.82%), Space: 20.6 MB (84.89%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,8 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d=defaultdict(list)
+
+        for i in strs:
+            s="".join(sorted(i))
+            d[s].append(i)
+        return list(d.values())
