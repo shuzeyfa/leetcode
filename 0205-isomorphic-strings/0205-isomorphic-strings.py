608222245bed08f802c1ad41de80e2eaf4d7d093
Huzeyfa suleyman <shuzeyfa4@gmail.com>
2026-02-28 12:39:38 +0300

    Time: 5 ms (51.63%), Space: 16.6 MB (100%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        indexS = [0] * 200
+        indexT = [0] * 200 
+        length = len(s)
+        
+        if length != len(t):
+            return False
+        
+        for i in range(length):
+            if indexS[ord(s[i])] != indexT[ord(t[i])]: 
+                return False
+            
+            indexS[ord(s[i])] = i + 1 
+            indexT[ord(t[i])] = i + 1
+        
+        return True  
